82a71f705f27eb289a9672754ca96de7b08ae09f
Kishan Kumar Singh <122109702+12kishansingh@users.noreply.github.com>
2024-03-29 15:26:53 +0530

    Update Generate Parentheses.cpp

--- Generate Parentheses.cpp
+++ Generate Parentheses.cpp
@@ -33,3 +33,34 @@ bool isvalid(string &s){
     }
 
 };
+// another method whis faster than the above code 
+
+class Solution {
+public:
+vector<string>ans;
+ void solve(string &curr,int n,int open,int close){
+            if(curr.length()==2*n){
+                 ans.push_back(curr);
+                return;
+            }
+            if(open<n){
+            curr.push_back('(');
+            solve(curr,n,open+1,close);
+            curr.pop_back();
+            }
+            if(close<open){
+            curr.push_back(')');
+            solve(curr,n,open,close+1);
+            curr.pop_back();
+            }
+            
+ }
+    vector<string> generateParenthesis(int n) {
+        //recursion will take care of it
+        int open=0,close=0;
+        string curr="";
+         solve(curr,n,open,close);
+         return ans;
+    }
+
+};
