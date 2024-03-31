a26bd6404102bb272102a0a503bed1e85af6515f
Kishan Kumar Singh <122109702+12kishansingh@users.noreply.github.com>
2024-03-31 11:56:49 +0530

    Update N-Queens.cpp

--- N-Queens.cpp
+++ N-Queens.cpp
@@ -39,3 +39,39 @@ public:
         return ans;
     }
 };
+// Approach 2 for less time complexity
+class Solution {
+public:
+    vector<vector<string>> ans;// using set to check
+    int N;
+    unordered_set<int> col,dia,antidia;
+    void solve(vector<string> board, int row) {
+        if (row >= N) { // means we've already filled queens in above rows
+            ans.push_back(board);
+            return;
+        }
+        for (int j = 0; j < N; j++) { // column
+            int rightdia=j+row;// sum remain constant
+            int leftdia=row-j;// diffrence remain constant 
+            if(col.find(j)!=col.end()|| dia.find(rightdia)!=dia.end()|| antidia.find(leftdia)!=antidia.end()){
+                continue;
+            }
+            col.insert(j);
+            dia.insert(rightdia);
+            antidia.insert(leftdia);
+            board[row][j]='Q';
+            solve(board,row+1);
+//backtracking 
+             col.erase(j);
+            dia.erase(rightdia);
+            antidia.erase(leftdia);
+            board[row][j]='.';
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board(n, string(n,'.')); // create board
+        N = n;
+        solve(board, 0); // row =0 start filling
+        return ans;
+    }
+};
