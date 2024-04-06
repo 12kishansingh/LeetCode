38bfdac65d8a3e332a4479bbf6b7bb42cb09c3b4
Kishan Kumar Singh <122109702+12kishansingh@users.noreply.github.com>
2024-04-06 11:33:43 +0530

    Update Minimum Remove to Make valid parenthesis.cpp

--- Minimum Remove to Make valid parenthesis.cpp
+++ Minimum Remove to Make valid parenthesis.cpp
@@ -1,3 +1,4 @@
+//Approach 1- use of stack 
 class Solution {
 public:
     string minRemoveToMakeValid(string s) {
@@ -26,5 +27,38 @@ public:
         }
         return ans;
         
+    }
+};
+//Approach 2- Check the array from left to right, remove characters that do not meet the property mentioned above, same idea in backward way.
+class Solution {
+public:
+    string minRemoveToMakeValid(string s) {
+        // left to right
+        int open=0;string ans="";
+        for(int i=0;i<s.length();i++){
+            if(s[i]>='a' && s[i]<='z')ans.push_back(s[i]);
+            else if(s[i]=='('){
+                ans.push_back(s[i]);open++;
+            }
+            else if(open>0){
+                ans.push_back(s[i]);
+                open--;
+            }
+        }
+        //right to left
+        int close=0;
+        string final_ans="";
+        for(int i=ans.length()-1;i>=0;i--){
+            if(ans[i]>='a'&& ans[i]<='z')final_ans.push_back(ans[i]);
+            else if(ans[i]==')'){
+                final_ans.push_back(ans[i]);close++;
+            }
+            else if(close>0){
+                final_ans.push_back(ans[i]);
+                close--;
+            }
+        }
+        reverse(final_ans.begin(),final_ans.end());
+        return final_ans;
     }
 };
