9c313b640a172de01f3acb4055b22f5c2e41aae5
Kishan Kumar Singh <122109702+12kishansingh@users.noreply.github.com>
2024-04-10 15:53:39 +0530

    Update Reveal Cards in increasing order.cpp

--- Reveal Cards in increasing order.cpp
+++ Reveal Cards in increasing order.cpp
@@ -20,3 +20,25 @@ public:
         
     }
 };
+// use of queue simply
+class Solution {
+public:
+    vector<int> deckRevealedIncreasing(vector<int>& deck) {
+        int n=deck.size();
+        sort(deck.begin(),deck.end());
+        queue<int>q;
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            q.push(i);
+        }
+        for(int i=0;i<n;i++){
+            int idx=q.front();
+            ans[idx]=deck[i];
+            q.pop();
+            if(!q.empty()){
+            q.push(q.front());
+            q.pop();
+            }
+        }return ans;
+    }
+};
