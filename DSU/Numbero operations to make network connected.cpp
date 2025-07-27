fb3d11d853464fbc1150326123a4c32d538d88fb
Kishan Kumar Singh <122109702+12kishansingh@users.noreply.github.com>
2025-07-27 11:44:54 +0530

    Update Number of operations to make network connected.cpp

--- DSU/Numbero operations to make network connected.cpp
+++ DSU/Numbero operations to make network connected.cpp
@@ -1,45 +1,45 @@
 class Solution {
 public:
-vector<int>parent,rank;
-int find(int x){
-    if(x==parent[x]){
-    return x;
-    }
-    return parent[x]=find(parent[x]);
-}
-void Union(int x,int y){
-    int xparent=find(x);
-    int yparent=find(y);
+    vector<int> parent, rank;
 
-    if(xparent==yparent){
-        return;
+    int find(int x) {
+        if (x == parent[x]) return x;
+        return parent[x] = find(parent[x]);  // Path compression
     }
-    if(rank[xparent]>rank[yparent]){
-       parent[yparent]=xparent;
-    }
-    else if(rank[xparent]<rank[yparent]){
-        parent[xparent]=yparent;
-    }
-    else {
-        parent[xparent]=yparent;
-        rank[yparent]++;
-    }
-}
 
-    int makeConnected(int n, vector<vector<int>>& connections) {
-        if(connections.size()<n-1)return -1;
+    void Union(int x, int y) {
+        int xp = find(x);
+        int yp = find(y);
+
+        if (xp == yp) return;
+
+        if (rank[xp] < rank[yp]) {
+            parent[xp] = yp;
+        } else if (rank[xp] > rank[yp]) {
+            parent[yp] = xp;
+        } else {
+            parent[yp] = xp;
+            rank[xp]++;
+        }
+    }
+
+    int countComponents(int n, vector<vector<int>>& edges) {
         parent.resize(n);
-        rank.resize(n,0);
-        for(int i=0;i<n;i++){
-            parent[i]=i;
+        rank.resize(n, 0);
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
         }
-        int comp=n;
-        for(auto &v:connections){
-            if(find(v[0])!=find(v[1])){
-                comp--;
-                Union(v[0],v[1]);
-            }
+
+        for (int i = 0; i < edges.size(); i++) {
+            Union(edges[i][0], edges[i][1]);
         }
-        return comp-1;
+
+        set<int> st;
+        for (int i = 0; i < n; i++) {
+            st.insert(find(i));
+        }
+
+        return st.size();  // Number of connected components
     }
 };
