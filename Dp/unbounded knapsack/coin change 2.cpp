601a84c320af1e16e39e96620740d536ff0f0f7b
Kishan Kumar Singh <122109702+12kishansingh@users.noreply.github.com>
2025-08-09 00:35:42 +0530

    Update coin change 2.cpp

--- Dp/unbounded knapsack/coin change 2.cpp
+++ Dp/unbounded knapsack/coin change 2.cpp
@@ -26,4 +26,32 @@ public:
         return (int)dp[n][amt];
     }
 };
+// correct
+class Solution {
+public:
+const long long mod= 100000000003; // added this only 
+    int change(int amt, vector<int>& coins) {
+          int n = coins.size();
+        if (amt == 0) return 1;
+        if (n == 0) return 0;
 
+       
+        vector<vector<long long int>> dp(n + 2, vector<long long int>(amt + 2, 0));
+
+       
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= amt; j++) {
+                if(coins[i-1] > j) {
+                    dp[i][j]= dp[i-1][j];
+                } else {
+                    dp[i][j] = (dp[i-1][j]+dp[i][j-coins[i-1]])%mod;
+                }
+            }
+        }
+        return (int)dp[n][amt];
+    }
+};
