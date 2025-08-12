1a7c6a8f00dd3378bac02b5299a7f8b984ea462c
Kishan Kumar Singh <122109702+12kishansingh@users.noreply.github.com>
2025-08-12 10:24:13 +0530

    Update max. path sum from any node to any.cpp

--- Dp/Dp on trees/max. path sum from any node to any.cpp
+++ Dp/Dp on trees/max. path sum from any node to any.cpp
@@ -1,1 +1,25 @@
 
+class Solution {
+public:
+    int solve(TreeNode* root, int &res) {
+        if (root == nullptr) return 0;
+
+        int l = solve(root->left, res);
+        int r = solve(root->right, res);
+
+        // Choose max path starting at current node
+        int temp = max(root->val + max(l, r), root->val);
+
+        // Max path passing through current node
+        int ans = max(temp, root->val + l + r);
+
+        res = max(res, ans);
+        return temp;
+    }
+
+    int maxPathSum(TreeNode* root) {
+        int res = INT_MIN; // fix: handle all-negative cases
+        solve(root, res);
+        return res;
+    }
+};
