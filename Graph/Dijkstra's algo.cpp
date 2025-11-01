b114f6aabcd1913ff9cefebb5eac49d750f90195
Kishan Kumar Singh <122109702+12kishansingh@users.noreply.github.com>
2025-11-01 20:06:10 +0530

    Add Dijkstra's algorithm implementation
    Implement Dijkstra's algorithm to find shortest paths from a source vertex.

--- Graph/Dijkstra's algo.cpp
+++ Graph/Dijkstra's algo.cpp
@@ -26,3 +26,41 @@ vector<int>dijkstra(int V,vector<vector<int>>adj,int S){
   
 }
 };
+
+//GFG
+class Solution {
+  public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        vector<vector<pair<int,int>>>adj(V);
+        for(auto x:edges){
+            int u=x[0],v=x[1],w=x[2];
+            adj[u].push_back({v,w});
+            adj[v].push_back({u,w});
+        }
+        vector<int>dis(V,INT_MAX);
+        dis[src]=0;
+        
+        // min heap: (distance,node)
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,src});
+        
+        while(!pq.empty()){
+            auto[d,u]=pq.top();pq.pop();
+            if(d>dis[u])continue;
+            
+            for(auto x: adj[u]){
+                int v=x.first;
+                int w=x.second;
+                if(dis[u]!=INT_MAX && dis[u]+w<dis[v]){
+                    dis[v]=dis[u]+w;
+                    pq.push({dis[v],v});
+                }
+            }
+        }
+        
+        for(int i=0;i<V;i++){
+            if(dis[i]==INT_MAX)dis[i]=-1;
+        }
+        return dis;
+    }
+};
